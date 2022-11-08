d2e9ea0ad0d9c9a1dd708af80fdce71b2fb74d7d
Boris <banofon@gmail.com>
2022-11-08 10:50:40 -0600

    refactor: create generic onboarding view controller
    this commit adds a generic onboarding viewcontroller that can be reused accross multiple screens

--- Bankey/Onboarding/OnboardingViewController.swift
+++ Bankey/Onboarding/OnboardingViewController.swift
@@ -10,13 +10,28 @@ import UIKit
 class OnboardingViewController: UIViewController {
     
     let stackView = UIStackView()
+    
     let imageView = UIImageView()
     let label = UILabel()
+    
+    let heroImageName : String
+    let titleText : String
     
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
         layout()
+    }
+    
+    init(heroImageName : String, titleText : String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
 
@@ -31,13 +46,14 @@ extension OnboardingViewController {
         //image
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleToFill
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: heroImageName)
         
         //label
         label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
         label.textAlignment = .center
-        label.text = "Banker is faster, easier to use and has a brand new look and feel that will make you feel like you are back in the 1989"
+        label.text = titleText
+        //"Banker is faster, easier to use and has a brand new look and feel that will make you feel like you are back in the 1989"
         label.font = UIFont.preferredFont(forTextStyle: .title3)
         
     }
